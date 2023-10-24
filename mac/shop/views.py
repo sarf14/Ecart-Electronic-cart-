14aa068723f2187bf7ceb1f3a5dfcc0310f43b3c
sarf14 <shaikhsarfaraz1426@gmail.com>
2023-10-24 15:08:44 +0530

    Updated version

--- mac/shop/views.py
+++ mac/shop/views.py
@@ -91,32 +91,28 @@ def checkout(request):
 from math import ceil
 
 
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
 def search(request):
     query = request.GET.get('search')
     allProds = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
 
-    if query and len(query) >= 4:  # Check if query is not None and has at least 4 characters
-        for cat in cats:
-            prodtemp = Product.objects.filter(category=cat)
-            prod = [item for item in prodtemp if searchMatch(query, item)]
-            n = len(prod)
-            nSlides = n // 4 + ceil((n / 4) - (n // 4))
-            if len(prod) != 0:
-                allProds.append([prod, range(1, nSlides), nSlides])
-    else:
-        params = {'msg': "Please make sure to enter a relevant search query"}
-        return render(request, 'shop/search.html', params)
-
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
     params = {'allProds': allProds, "msg": ""}
-    if len(allProds) == 0:
-        params = {'msg': "No products found for the search query"}
+    if len(allProds) == 0 or len(query)<4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
 
-    return render(request, 'shop/index.html', params)
-
-
-def searchMatch(query, item):
-    if query in item.product_name or query in item.category:
-        return True
-    return False
